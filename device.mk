db0c362c32bd24ebb44317e8e7f09fb07f7ae3b4
AzariahVidya <leonardo.luzzi@gmail.com>
2013-12-04 15:46:06 +0100

    OmniTorch for Nexus 5 (hammerhead)
    Refs from CM
    
    Patchset 3: - add some more information to the overlay
                - put the call in device.mk back to where it was originally
    Patchset 4: fix spacing
    Patchset 5: Change permission of sysfs parameter
    
    Change-Id: I07f6be272d57d228f2ca32804cf52c4e770e7abd

--- device.mk
+++ device.mk
@@ -186,9 +186,9 @@ PRODUCT_PACKAGES += \
     gps.msm8974 \
     flp.msm8974
 
-# OmniTorch
-PRODUCT_PACKAGES += \
-  OmniTorch
+# OmniTorch
+PRODUCT_PACKAGES += \
+    OmniTorch
 
 # NFC packages
 PRODUCT_PACKAGES += \
